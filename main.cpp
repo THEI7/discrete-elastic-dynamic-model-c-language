056077b9d15162753b27de7f2778289279621208
THEI7 <92933421+THEI7@users.noreply.github.com>
2024-03-01 06:25:01 +0800

    函数功能基本实现

--- main.cpp
+++ main.cpp
@@ -13,30 +13,29 @@ using namespace std;
 
 typedef struct Elastic_system_model{
     /*user set*/
-    float mass;
-    float stiffnes;
-    float damping;
-    float tar;
-    float now;
-    float last;
-    float v0;
-    float a;
-    float T;
+    float mass;//重量
+    float stiffnes;//刚力
+    float damping;//阻尼
+    float obj_tar;//目标位置
+    float obj_now;//当前位置
+    float obj_last;//上一帧的位置
+    float v0;//初始速度
+    float a;//加速度
+    float T;//每一帧之间的时间间隔
 }ElasticSystemModel_t;
 
 ElasticSystemModel_t  esm = {
-        .mass = 2,
-        .stiffnes = 1,
-        .damping = 0.1,
-        .tar = 200,
-        .now = 0,
-        .last = 0,
-        .v0 = 0,
-        .a = 0,
-        .T = 1,
+        .mass = 2,//重量
+        .stiffnes = 1,//刚力
+        .damping = 0.1,//阻尼
 
+        .obj_tar = 200,//目标位置
+        .obj_now = 0,//当前位置
+        .obj_last = 0,//上一帧的位置
 
-
+        .v0 = 0,//初始速度
+        .a = 0,//加速度
+        .T = 1,//每一帧之间的时间间隔
 };
 
 float Elastic_system_model_update(ElasticSystemModel_t * elastic)
@@ -44,14 +43,14 @@ float Elastic_system_model_update(ElasticSystemModel_t * elastic)
     static float output = 0.0f;
     //1. 计算上一帧的信息
     elastic->v0=elastic->v0+ elastic->a*elastic->T;
-    elastic->a =(  ( (elastic->tar - elastic->now) * elastic->stiffnes ) - (elastic->mass * GRAVITY * elastic->damping)   )/ elastic->mass;
+    elastic->a =(  ( (elastic->obj_tar - elastic->obj_now) * elastic->stiffnes ) - (elastic->mass * GRAVITY * elastic->damping)   )/ elastic->mass;
 //    if((elastic->mass * GRAVITY * elastic->damping) >= ((elastic->tar - elastic->now) * elastic->stiffnes )) elastic->a = 0;//小球系统近似于谐振子系统
     //2. 计算当前帧
-    elastic->now = (elastic->v0 * elastic->T) + (elastic->a * elastic->T * elastic->T / 2);
-    output = elastic->now;
+    elastic->obj_now = (elastic->v0 * elastic->T) + (elastic->a * elastic->T * elastic->T / 2);
+    output = elastic->obj_now;
 
-    elastic->last = elastic->now;
-    printf("v0:%f a:%f  last:%f now:%f\r\n",elastic->v0,elastic->a,elastic->last,elastic->now);
+    elastic->obj_last = elastic->obj_now;
+    printf("v0:%f a:%f  last:%f now:%f\r\n",elastic->v0,elastic->a,elastic->obj_last,elastic->obj_now);
     return output;
 }
 float outputt  = 0.0f;
